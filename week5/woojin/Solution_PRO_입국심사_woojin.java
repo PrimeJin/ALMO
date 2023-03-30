f2d4626fa9f1971c2ea81851f7c0be43c2cb5551
조우진 <sky5962146@gmail.com>
2023-03-31 00:45:01 +0900

    Add week5 Solution_PRO_입국심사_woojin

--- week5/woojin/Solution_PRO_입국심사_woojin.java
+++ week5/woojin/Solution_PRO_입국심사_woojin.java
@@ -6,17 +6,17 @@ class Solution {
         int len = times.length;
         
         Arrays.sort(times);
-        int lt = 0;
-        int rt = times[len-1] * n;
+        long lt = 0;
+        long rt = (long)times[len-1] * n;  //최악의 경우(걸리는 시간)
         
-        while(lt<rt) {
-            int mid = (lt+rt)/2;
-            int sum = 0;
+        while(lt<=rt) {
+            long mid = (lt+rt)/2;
+            long sum = 0;  //심사 처리한 사람 수
             for(int i=0; i<len; i++) {
                 sum += mid/times[i];
             }
-            if(sum > n) {
-                rt = mid-1;
+            if(sum >= n) {  //mid 동안 처리한 사람수가 같거나 더 많은 경우
+                rt = mid-1;  //왼쪽 절반으로 범위 축소
                 answer = mid;
             } else {
                 lt = mid+1;
