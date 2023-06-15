577f73603f8b039ea8a195182e77671318c2821c
여민지 <yeomj051@naver.com>
2023-06-15 16:32:47 +0900

    Add week13 Solution_PRO_모의고사1회_1외톨이알파벳_minji

--- week13/minji/Solution_PRO_모의고사1회_1외톨이알파벳_minji.java
+++ week13/minji/Solution_PRO_모의고사1회_1외톨이알파벳_minji.java
@@ -1,2 +1,26 @@
-package week13.minji;public class Solution_PRO_모의고사1회_1외톨이알파벳_minji {
+class Solution {
+    public String solution(String input_string) {
+        String answer = "";
+        int[] alpha = new int[26];
+        int count=1;
+
+        for(char spell: input_string.toCharArray()){
+            int spellIdx = alpha[(int)(spell-'a')];
+            // System.out.println(spell + " " + count + " " + spellIdx);
+
+            if(spellIdx == 0 || (count - spellIdx) == 1) alpha[(int)(spell-'a')] = count;
+            else alpha[(int)(spell-'a')] = -1;
+
+            count++;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i=0; i<26; i++){
+            if(alpha[i] == -1) stringBuilder.append((char)('a'+i));
+        }
+
+        answer = stringBuilder.toString();
+        if(answer.equals("")) answer="N";
+        return answer;
+    }
 }
---
+++ week13/minji/Solution_PRO_모의고사1회_2체육대회_minji.java
@@ -0,0 +1,2 @@
+package week13.minji;public class Solution_PRO_모의고사1회_2체육대회_minji {
+}
