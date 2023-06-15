365d037ce9cfe0895c020d66d0c485eb349387a8
여민지 <yeomj051@naver.com>
2023-06-15 16:33:51 +0900

    Add week13 Solution_PRO_모의고사1회_2체육대회_minji

--- week13/minji/Solution_PRO_모의고사1회_2체육대회_minji.java
+++ week13/minji/Solution_PRO_모의고사1회_2체육대회_minji.java
@@ -1,2 +1,31 @@
-package week13.minji;public class Solution_PRO_모의고사1회_2체육대회_minji {
+import java.lang.*;
+
+class Solution {
+    public static int object;
+    public static int student;
+    public static int max;
+
+    public int solution(int[][] ability) {
+        int answer = 0;
+        object = ability[0].length;
+        student = ability.length;
+
+        dfs(0, 0, ability,0);
+
+        answer = max;
+        return answer;
+    }
+
+    public static void dfs(int count, int sum, int[][] ability, int mask){
+        if(count == object){
+            max = Math.max(max,sum);
+            return;
+        }
+
+        for(int i=0; i<student;i++){
+            if((mask & (1<<i+1)) > 0) continue;
+
+            dfs(count+1, sum+ability[i][count],ability, mask+(1<<i+1));
+        }
+    }
 }
